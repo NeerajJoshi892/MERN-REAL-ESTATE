ce7dce50b1c302d66ee84b7d5442a7ebc308430c
neeraj joshi <neerajjoshi892@gmail.com>
2024-07-25 09:26:55 +0530

    install react js and tailwind css and create a first template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-300 backdrop-blur-lg'>
+       this is our real state project
+    </div>
+  )
+}
+
+export default App
